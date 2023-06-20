f1a2fa2dbc8098b211519e4bf17ebde4b3b709ec
boboyevnurbek <boboyevnurbek95@gmail.com>
2023-06-20 19:31:47 +0500

    COLLECTIONS -> MAPS

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,17 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
-        Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+        return calculateFunctionMap(sourceList).containsValue(requiredValue);
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        sourceList.forEach(x -> map.put(x, 5 * x + 2));
+        return map;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,17 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> map = new HashMap<>();
+
+        sourceMap.forEach((key, value) -> {
+            map.merge(value, key, Integer::min);
+        });
+
+        return map;
     }
 }
